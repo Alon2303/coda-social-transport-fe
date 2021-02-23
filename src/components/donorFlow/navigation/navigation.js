760e29a6b3d03c14135cc8aafa08be4334f9ebb9
innaya12 <yakubov.inna2@gmail.com>
2021-02-23 20:33:59 +0200

    navigation bar + mumenu

--- src/components/donorFlow/navigation/navigation.js
+++ src/components/donorFlow/navigation/navigation.js
@@ -1,32 +1,33 @@
-import { MenuList, MenuItem } from '@material-ui/core';
-import {Link} from 'react-router-dom';
-import React, { Component } from 'react';
+import React from 'react';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import {  createMuiTheme , ThemeProvider} from '@material-ui/core/styles';
+import MyMenu from '../menu/mymenu';
 
-class Navigation extends Component{
-    state={
+const theme = createMuiTheme ({
+    direction: 'rtl',
+    typography: {
+        fontFamily: 'Rubik',
+        fontSize: 14
+    },
+});
 
-    }
-    render (){
-    const menush = (
-        <div>
-            <MenuList>
-                <MenuItem component={Link} to={'/signin'}>
-                Home1
-                </MenuItem>
-                <MenuItem component={Link} to={'/signup'}>
-                Home2
-                </MenuItem>
-            </MenuList>
-        </div>
 
-    )
+const Navigation = () =>{
+  return (
+  <div>
+      <ThemeProvider theme={theme}>
+      <Toolbar style={{position:'absolute', top:'56px'}}>
+            <Typography style={{position:'absolute', zIndex:'9999', backgroundColor:'#56959D', borderRadius:'30px 0 0 30px',padding:'14px 30px'}}> פריטים</Typography>
+            <Typography style={{ position:'relative',left:'-85px',  backgroundColor:'#88AD3F', borderRadius:'30px 0 0 30px',padding:'14px 30px', color:"white"}} > הובלה</Typography>
+            <Typography style={{position:'relative', left:'-74px',borderLeft:"1px solid lightgray",borderRadius:'30px 0 0 30px',padding:'14px 30px'}}> פריטים</Typography>
+            <Typography style={{position:'relative', left:'-74px',padding:'14px 30px'}}> הובלה</Typography>
+        </Toolbar>
+        </ThemeProvider>
 
-    return (
-        <div>
-        </div>
-
-    )
-    }
+        <MyMenu/>
+    </div>
+  );
 }
 
 export default Navigation;
